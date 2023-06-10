03d4788c3495b15248604df0623481c325a5cc36
willb <willbarrera9@gmail.com>
2023-06-10 12:30:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            cardName: 'Dark Magician', 
+            cardImg: 'https://images.ygoprodeck.com/images/cards_small/46986421.jpg'
+        }
+    },
+    methods: {
+        searchCard(){
+            this.cardName = document.getElementById("inputCard")
+        }
+    }
+})
+
+app.mount('#app')
