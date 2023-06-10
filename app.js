4036373ac556865c9dbeeb77cc135c938af125c2
willb <willbarrera9@gmail.com>
2023-06-10 12:40:11 -0500

    Added comments on planned features to index.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data(){
         return{
+            showInfo: false,
             cardName: 'Dark Magician', 
             cardImg: 'https://images.ygoprodeck.com/images/cards_small/46986421.jpg'
         }
