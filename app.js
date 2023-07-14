0715cdb50be174b0c2ba5a762fdeeb7d023a9fc8
willb <willbarrera9@gmail.com>
2023-07-14 08:44:42 -0500

    api requests working, work on vue features

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ const app = Vue.createApp({
     data(){
         return{
             showInfo: false,
-            cardName: 'Dark Magician', 
-            cardImg: 'https://images.ygoprodeck.com/images/cards_small/46986421.jpg'
+            cardName: '', 
+            cardImg: ''
         }
     },
     methods: {
         searchCard(){
             this.cardName = document.getElementById("inputCard").value
-            console.log(this.cardName)
+        
+            const ygoxhr= new XMLHttpRequest
+            ygoxhr.open('GET','https://db.ygoprodeck.com/api/v7/cardinfo.php?name='+this.cardName)
+            ygoxhr.onload = function () {
+                let info = ygoxhr.response
+                console.log("info returned"+info)
+
+                let printinfo = JSON.parse(info).data[0]
+                //parsed info//
+                console.log(printinfo)
+                console.log(printinfo.name)
+                console.log(printinfo.card_images[0].image_url_small)
+                // TO DO: format parsed info so it can be displayed on the home page.//
+                this.cardName = printinfo.name
+                this.cardImg = printinfo.card_images[0].image_url_small
+
+                document.getElementById("ygoimg").src = printinfo.card_images[0].image_url_small
+                document.getElementById("ygoname").innerHTML = "Card Name: "+printinfo.name
+                document.getElementById("ygoid").innerHTML = "Card ID: "+printinfo.id
+                document.getElementById("ygodesc").innerHTML = "Card Description: "+printinfo.desc
+            }
+            ygoxhr.send()
             this.showInfo = !this.showInfo
         }
     }
